2a989ba4af6317edeede2b1f46b6fa31d4c91cee
fafato1 <fafato.889@gmail.com>
2019-09-26 16:00:22 -0300

    Please, someone help me :sob:

--- assets/html/trabalho.js
+++ assets/html/trabalho.js
@@ -3,39 +3,84 @@ var jogador = {
     cliques: 0,
 };
 
+var luz = false, luz1 = false, luz2 = false, luz3 = false;
+var luzesAcesas = 0;
+
 
 function Acenderluz(){
-    document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulbon.gif'
+    if(luz === false){
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        luz = true;
+        luzesAcesas += 1;
+        jogador.cliques += 1;
+        jogador.pontuacao += 5;
+    }
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
+
+    (luz === true && luz1 === true && luz2 === true && luz3 === true) ? window.alert("Parabens você concluiu em " + jogador.cliques + " cliques"): "" ;
 }
 
 
-function Acenderluz1(){
-    document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulbon.gif'
-    document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulboff.gif'
-    document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
+function Acenderluz1(){ //Apaga 4 e 1
+    if(luz1 === false){
+        document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulboff.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz1 = true;
+        luz3 = false;
+        luz = false;
+        jogador.cliques += 1;
+        jogador.pontuacao += 5;
+        (luzesAcesas % 2 == 0) ? jogador.pontuacao -= 6 : jogador.pontuacao -= 4;
+    }
+
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 }
 
 function Acenderluz2(){ //Apaga todos
-    document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulbon.gif'
-    document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulboff.gif'
-    document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulboff.gif'
-    document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
+    if(luz2 === false){
+        document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz2 = true;
+        luz = false;
+        luz1 = false;
+        luz3 = false;
+
+        luzesAcesas += 1;
+        jogador.cliques += 1;
+        jogador.pontuacao += 5;
+        (luzesAcesas % 2 == 0) ? jogador.pontuacao -= 9 : jogador.pontuacao -= 6;
+    }
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 }
 
-function Acenderluz3(){
-    document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulbon.gif'
+function Acenderluz3(){ //Apaga 1
+    if(luz3 === false){
+        document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz3 = true;
+        luz = false;
+
+        luzesAcesas += 1;
+        jogador.cliques += 1;
+        jogador.pontuacao += 5;  
+        (luzesAcesas % 2 == 0) ? jogador.pontuacao -= 3 : jogador.pontuacao -= 2;
+    }
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
     
 }
 
@@ -43,33 +88,79 @@ function Acenderluz3(){
 
 
 function Apagarluz(){
-    document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
+    if(luz === true){
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulbon.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz = false;
+        luz1 = true;
+
+        console.log("luz = false, luz1 = true");
+
+        jogador.cliques += 1;
+    }
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 
 }
 
-function Apagarluz1(){
-    document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulboff.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+
+
+function Apagarluz1(){
+    if(luz1 === true){
+        document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        document.getElementById("myImage").src='https://www.w3schools.com/js/pic_bulbon.gif'
+
+        luz1 = false;
+        luz = true;
+        luz2 = true;
+        console.log("luz1 = false, luz = true, luz2 = true");
+
+        jogador.cliques += 1;
+    }
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 
 }
 
 function Apagarluz2(){
-    document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulboff.gif'
+    if(luz2 === true){
+        document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulbon.gif'
+        document.getElementById("myImage1").src='https://www.w3schools.com/js/pic_bulbon.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz2 = false;
+        luz1 = true;
+        luz3 = true;
+        console.log("luz2 = false, luz1 = true, luz3 = true");
+
+        jogador.cliques += 1;
+    }
+
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 
 }
 
 function Apagarluz3(){
-    document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulboff.gif'
+    if(luz3 === true){
+        document.getElementById("myImage3").src='https://www.w3schools.com/js/pic_bulboff.gif'
+        document.getElementById("myImage2").src='https://www.w3schools.com/js/pic_bulbon.gif'
 
-    jogador.cliques = jogador.cliques + 1;
-    document.getElementById("cliques").innerHTML = jogador.cliques;
+        luz3 = false;
+        luz2 = true;
+        console.log("luz3 = false, luz2 = true");
+
+        jogador.cliques += 1;
+    }
+
+    document.getElementById("cliques").innerHTML = "Cliques: " + jogador.cliques;
+    document.getElementById("luzes").innerHTML = "Luzes acesas: " + luzesAcesas;
+    document.getElementById("pontos").innerHTML = "Pontos: " + jogador.pontuacao;
 
 }
